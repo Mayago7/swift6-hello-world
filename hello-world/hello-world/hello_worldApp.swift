909cfb322b28c943e9f4943ae0e7b15549a45450
Bernardo Cervantes Mayagoitia <36172359+Mayago7@users.noreply.github.com>
2024-10-07 11:54:45 -0600

    Add files via upload

---
+++ hello-world/hello-world/hello_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_worldApp.swift
+//  hello-world
+//
+//  Created by Bernardo Cervantes Mayagoitia on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct hello_worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
